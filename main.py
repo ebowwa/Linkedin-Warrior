f75193c62b30424fb7c46525424e99c03fc80c09
ebowwa <81942069+ebowwa@users.noreply.github.com>
2022-12-08 03:45:58 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from linkedin import Linkedin
 from utils import log
 from constants import BOT_SPEED, PHONE_VERIFICATION_DELAY
+
+# initialize the LinkedIn bot
+bot = Linkedin()
+
+# wait for the user to verify their phone number
+time.sleep(PHONE_VERIFICATION
